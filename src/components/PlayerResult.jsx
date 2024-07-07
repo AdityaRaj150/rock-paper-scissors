9afed47a6f96ed337bd2490b9c1b2b7f3875e6f4
“AdityaRaj150” <adityaraj10544@gmail.com>
2024-07-07 15:33:53 +0530

    overflow-hidden removed

--- src/components/PlayerResult.jsx
+++ src/components/PlayerResult.jsx
@@ -4,7 +4,7 @@ import drawImg from "../assets/draw.svg"
 export default function PlayerResult({name,win, loss, draw}){
         const nameToShow = name.substring(0, Math.min(name.length-1, 30))
         return <div className="rounded bg-stone-800 w-full m-auto p-10 text-xs sm:text-sm flex flex-col justify-center items-center">
-            <div className="w-[60%] overflow-hidden max-[380px]:w-[70%] flex flex-col justify-start gap-7" >
+            <div className="w-[60%]  max-[380px]:w-[70%] flex flex-col justify-start gap-7" >
                 <h1 className="text-center max-w-full text-slate-100 text-sm sm:text-lg">{nameToShow}</h1>
                 <div className="min flex gap-2 justify-between " >
                     <h2 className="text-green-400 w-1/3" >Win</h2>
