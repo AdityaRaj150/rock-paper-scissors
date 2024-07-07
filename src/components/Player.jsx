c5ed97e1f755bb079f0dec8f3bd6069ddca3646f
“AdityaRaj150” <adityaraj10544@gmail.com>
2024-07-07 16:02:15 +0530

    enhanced accessibility

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -15,7 +15,8 @@ export default function Player({playerName,playerNumber,changePlayerName}){
     }
 
     return <div className="flex w-fit gap-2 min-[500px]:gap-3 bg-zinc-200 p-2 min-[500px]:p-4 rounded">
-        <input className="bg-stone-800 max-[500px]:w-64  rounded py-2 px-4 min-[500px]:py-1 min-[500px]:px-2 text-slate-100" value={playerNameState} onChange={isEditing? handleOnchange: null} readOnly={!isEditing}  />
+        <label htmlFor="playerinput" />
+        <input id="playerinput" className="bg-stone-800 max-[500px]:w-64  rounded py-2 px-4 min-[500px]:py-1 min-[500px]:px-2 text-slate-100" value={playerNameState} onChange={isEditing? handleOnchange: null} readOnly={!isEditing}  />
         <button className={`rounded duration-150 ease-linear active:scale-95 hover:shadow-lg py-1 px-2 min-[500px]:py-2 min-[500px]:px-4 ${isEditing? "bg-green-400 hover:shadow-green-200": "bg-cyan-300 hover:shadow-cyan-200"}`}  onClick={handleOnClick} >{isEditing? "Save": "Edit"}</button>
     </div>
 }
